a61fb27a0ca5695e1b89106cc5f2d27c76a53574
trongpv93 <trongpv93@gmail.com>
2022-05-04 10:26:46 +0700

    d

---
+++ web/.quasar/quasar-user-options.js
@@ -0,0 +1,22 @@
+/**
+ * THIS FILE IS GENERATED AUTOMATICALLY.
+ * DO NOT EDIT.
+ *
+ * You are probably looking on adding startup/initialization code.
+ * Use "quasar new boot <name>" and add it there.
+ * One boot file per concern. Then reference the file(s) in quasar.config.js > boot:
+ * boot: ['file', ...] // do not add ".js" extension to it.
+ *
+ * Boot files are your "main.js"
+ **/
+
+import iconSet from 'quasar/icon-set/material-icons'
+
+
+
+import {Dark,Notify,Dialog} from 'quasar'
+
+
+
+export default { config: {},iconSet,plugins: {Dark,Notify,Dialog} }
+
